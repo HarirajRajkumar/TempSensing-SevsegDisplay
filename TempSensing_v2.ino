23257a3c8cd15d7c959d3987f7a7aeac85ef5d0b
Hariraj Rajkumar <harirajkumar1997@gmail.com>
2016-07-23 20:02:36 +0530

    Add files via upload

---
+++ TempSensing_v2.ino
@@ -0,0 +1,20 @@
+#include <SevSeg.h>
+
+byte num=123;
+ SevSeg sevseg; //Instantiate a seven segment object
+ 
+ void setup() {
+   byte numDigits = 4;
+   byte digitPins[] = {5 , 4 , 3 ,2 };
+   byte segmentPins[] = {6, 7, 8,9, 10,11,12, 13};
+   sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+ }
+
+ void loop() {
+  sevseg.setNumber(num,2); // ...ber( number , decimal point )
+  //sevseg.refreshDisplay();
+  
+ 
+  
+ }
+
